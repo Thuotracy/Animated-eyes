de006e0a75a3f981b6b9a6db72b37fd731c84a45
Thuotracy <tracyjacobs775@gmail.com>
2022-09-03 15:55:46 +0300

    called a function eyeball

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('body').addEventListener('mousemove', eyeball);
+
+function eyeball(){
+    const eye = document.querySelectorAll('.eye');
+    eye.forEach(function(eye){
+        let x 
+    })
+}
